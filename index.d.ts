814e64188e175e4bab9e688e92184300645aa658
Marco Buono <thecoreh@gmail.com>
2017-07-04 23:55:54 -0300

    Make all hook types generic

--- index.d.ts
+++ index.d.ts
@@ -2,21 +2,21 @@ import * as feathers from 'feathers';
 
 declare module 'feathers' {
   interface Application {
-    hooks(hooks: hooks.HooksObject): Application;
+    hooks(hooks: hooks.HooksObject<{}>): Application;
   }
 
   interface Service<T> {
-    before(hooks: hooks.HookMap): Application;
-    after(hooks: hooks.HookMap): Application;
-    hooks(hooks: hooks.HooksObject): Application;
+    before(hooks: hooks.HookMap<T>): Application;
+    after(hooks: hooks.HookMap<T>): Application;
+    hooks(hooks: hooks.HooksObject<T>): Application;
   }
 }
 
 declare function hooks(): () => void;
 
 declare namespace hooks {
-  interface Hook {
-    <T>(hook: HookProps<T>): Promise<any> | void;
+  interface Hook<T> {
+    (hook: HookProps<T>): Promise<any> | void;
   }
 
   interface HookProps<T> {
@@ -28,20 +28,20 @@ declare namespace hooks {
     app?: feathers.Application;
   }
 
-  interface HookMap {
-    all?: Hook | Hook[];
-    find?: Hook | Hook[];
-    get?: Hook | Hook[];
-    create?: Hook | Hook[];
-    update?: Hook | Hook[];
-    patch?: Hook | Hook[];
-    remove?: Hook | Hook[];
+  interface HookMap<T> {
+    all?: Hook<T> | Hook<T>[];
+    find?: Hook<T> | Hook<T>[];
+    get?: Hook<T> | Hook<T>[];
+    create?: Hook<T> | Hook<T>[];
+    update?: Hook<T> | Hook<T>[];
+    patch?: Hook<T> | Hook<T>[];
+    remove?: Hook<T> | Hook<T>[];
   }
 
-  interface HooksObject {
-    before?: HookMap;
-    after?: HookMap;
-    error?: HookMap;
+  interface HooksObject<T> {
+    before?: HookMap<T>;
+    after?: HookMap<T>;
+    error?: HookMap<T>;
   }
 }
 
